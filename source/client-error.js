431a501e6cfa3ca5b594c657e8f24a4c3b2bd73f
Julie Ng <me@julie.io>
2020-01-23 13:47:08 +0100

    error: distinguish between server and standard errors

--- source/client-error.js
+++ source/client-error.js
@@ -2,17 +2,10 @@ class ClientError {
 	constructor (err) {
 		// console.log(err)
 
-		const msg = friendly.hasOwnProperty(err.statusCode)
-			? friendly[err.statusCode]
-			: friendly.default
-
-		return {
-			code: err.response.parsedBody.Code,
-			message: msg,
-			details: {
-				statusCode: err.statusCode,
-				response: err.response.parsedBody.message
-			}
+		if (err.response) {
+			return _serverError(err)
+		} else {
+			return err
 		}
 	}
 }
@@ -22,6 +15,21 @@ const friendly = {
 	403: 'Please check your credentials.',
 	409: 'Cannot create container. Please choose a different name.',
 	default: 'Miscellanous error. Debug server response for details.'
+}
+
+function _serverError (err) {
+	const msg = friendly.hasOwnProperty(err.statusCode)
+		? friendly[err.statusCode]
+		: friendly.default
+
+	return {
+		code: err.response.parsedBody.Code,
+		message: msg,
+		details: {
+			statusCode: err.statusCode,
+			response: err.response.parsedBody.message
+		}
+	}
 }
 
 module.exports = ClientError
